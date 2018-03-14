93a89a79f0f7d70b4f3a746f8738ed226c4f15a3
gadiza <gadizaz@hotmail.com>
2018-03-14 15:57:05 +0000

    Loading data as argument in command line

--- directory.rb
+++ directory.rb
@@ -3,20 +3,20 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the sudent hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 def print_menu
@@ -70,7 +70,7 @@ def save_students
   end
   file.close
 end
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
@@ -78,5 +78,17 @@ def load_students
   end
   file.close
 end
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil?#get out of the method if it isn't given
+  if File.exists?(filename)#if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quit the program
+  end
+end
 #nothing will happen until we call the interactive_menu
+try_load_students
 interactive_menu
