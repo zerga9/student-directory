cd8cf9158a3363aea15dde32930880596116817d
gadiza <gadizaz@hotmail.com>
2018-03-14 10:03:31 +0000

    8.ex. number before student

--- directory.rb
+++ directory.rb
@@ -21,13 +21,14 @@ def print_header
   puts "-------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
 #nothing will happen until we call the methods
 students = input_students
 print_header
